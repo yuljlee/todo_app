9a67844114e7e6d8e3fb2feaaf47ee7eba5bbec2
Yul Jae Lee <yuljlee9@gmail.com>
2020-01-16 23:50:19 -0800

    init commit

---
+++ model/todo.dart
@@ -0,0 +1,9 @@
+class Todo {
+  int _id;
+  String _title;
+  String _description;
+  String _date;
+  int _priority;
+
+  Todo(this._id, this._title, this._date, this._priority, [this._description]);
+}
