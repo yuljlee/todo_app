14d0a557fbc026a53352c4d6b4a3d8059a69bee7
Yul Jae Lee <yuljlee9@gmail.com>
2020-01-17 00:08:14 -0800

    feat: Add todo class constructor

--- model/todo.dart
+++ model/todo.dart
@@ -5,5 +5,6 @@ class Todo {
   String _date;
   int _priority;
 
-  Todo(this._id, this._title, this._date, this._priority, [this._description]);
+  Todo(this._title, this._date, this._priority, [this._description]);
+  Todo.withId(this._id, this._title, this._date, this._priority, [this._description]);
 }
